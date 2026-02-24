b1b76f17fae42117e24349e1236027357feee7e5
Raw Fish <zach.otto.schmalz@gmail.com>
2026-02-23 19:10:10 -0700

    It's supposed to be the whole thing...

---
+++ src/main/java/net/toxicgold180/many_soils/ManySoils.java
@@ -0,0 +1,16 @@
+package net.toxicgold180.many_soils;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ManySoils implements ModInitializer {
+	public static final String MOD_ID = "many_soils";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/toxicgold180/many_soils/ManySoilsClient.java
@@ -0,0 +1,10 @@
+package net.toxicgold180.many_soils;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ManySoilsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
